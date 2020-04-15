294e882d80f0442a2cba7e59bfcdd19fea14f374
jviaud <55094662+jviaud@users.noreply.github.com>
2020-04-15 17:38:04 -0400

    changed app icon

--- app/src/main/java/com/example/cs491_capstone/services/AppChangeListener.java
+++ app/src/main/java/com/example/cs491_capstone/services/AppChangeListener.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import static com.example.cs491_capstone.App.getInstalledApps;
+
 public class AppChangeListener extends BroadcastReceiver {
 
     private Context context;
@@ -15,16 +17,17 @@ public class AppChangeListener extends BroadcastReceiver {
         switch (intent.getAction()) {
             case Intent.ACTION_PACKAGE_ADDED:
             case Intent.ACTION_PACKAGE_REMOVED:
-                generateNewAppList();
+                generateNewAppList(context);
                 break;
         }
 
     }
 
 
-    private void generateNewAppList() {
+    private void generateNewAppList(Context context) {
         //TODO GENERATE NEW LIST OF APPS WHEN A NEW APP IS INSTALLED, THIS WAY WE DO NOT HAVE TO GENERATE IT IN ONRESUME
         Log.i("BROADCASTING", "RECEIVED");
+        getInstalledApps(context);
     }
 
 
